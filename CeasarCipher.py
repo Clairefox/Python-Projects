1df4b314262ac4765efc561221c76bcdd2fec1d1
Clairefox <claire.2007@live.com>
2021-03-05 15:18:50 -0500

    Initial creation. Numbers and punctuation are preserved. Alphabet characters are converted to uppercase and shifted..

---
+++ CeasarCipher.py
@@ -0,0 +1,37 @@
+#What is a Ceasar cipher? https://en.wikipedia.org/wiki/Caesar_cipher
+
+#Initialize Variables
+plainText = (input("Please enter a sentence to encrypt:\n")).upper()
+
+shiftValid = False
+while (shiftValid == False):
+    shift = int(input("Please enter a number by which to 'shift' each letter from 1 to 26:\n"))
+    shiftValid = bool(1<= int(shift) <= 26)
+
+print("Original string:\n", plainText)
+
+#Convert each chr to its ASCII value
+plainASCII = []
+for _ in plainText:
+    plainASCII.append(ord(_))
+
+print("Original string in ASCII code:\n", plainASCII)
+
+#Shift each ASCII value
+shiftedASCII = []
+for _ in plainASCII:
+    if (65 <= _ <= (90 - shift)):
+        shiftedASCII.append(_ + shift)
+    elif ((90 - shift) < _ <= 90):
+        shiftedASCII.append(64 + (shift + (90 - _)))
+    else:
+        shiftedASCII.append(_)
+
+print("Encrypted string in ASCII code:\n", shiftedASCII)
+
+#Convert each ASCII value back to chr
+encryptedASCII = ""
+for _ in shiftedASCII:
+    encryptedASCII += chr(_)
+
+print("Encrypted string:\n", encryptedASCII)
